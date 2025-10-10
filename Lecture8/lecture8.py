d2b40045e1cd6764928f24f00a9b9f729d6a1d45
Alperen <125186840+alprn061@users.noreply.github.com>
2025-10-10 11:17:37 +0300

    functions

---
+++ Lecture8/lecture8.py
@@ -0,0 +1,11 @@
+# What does this print to the console? 
+# Think first, then run it. 
+def add(x,y):
+    return x+y
+def mult(x,y):
+    print(x*y)
+
+add(1,2)
+print(add(2,3))
+mult(3,4)
+print(mult(4,5))
